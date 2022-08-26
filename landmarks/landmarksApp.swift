b1f68728b7a6fb5d80f004aca3585b0a6945293f
michaeldadzie <dadziemikke2@gmail.com>
2022-08-26 20:00:33 +0000

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Michael Dadzie on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

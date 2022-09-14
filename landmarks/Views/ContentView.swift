a6a832a8cf2562d35007ac16fd7dc8f9cfb1f78d
Michael Dadzie <dadziemikke2@gmail.com>
2022-09-14 22:28:56 +0000

    Refactored code

--- landmarks/Views/ContentView.swift
+++ landmarks/Views/ContentView.swift
@@ -29,5 +29,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .environmentObject(ModelData())
+            .previewInterfaceOrientation(.portrait)
     }
 }
